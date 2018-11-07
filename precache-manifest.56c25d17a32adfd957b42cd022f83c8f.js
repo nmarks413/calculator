80237bbf6514cc19c0404ed790e9acbe0367ccc2
nmarks413 <nmarks413@gmail.com>
2018-11-06 23:32:24 -0800

    Updates

---
+++ precache-manifest.56c25d17a32adfd957b42cd022f83c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "2ceb23c98c95d52fce43",
+    "url": "/calculator/static/js/main.2ceb23c9.chunk.js"
+  },
+  {
+    "revision": "fd97677c7b9454c5e64f",
+    "url": "/calculator/static/js/1.fd97677c.chunk.js"
+  },
+  {
+    "revision": "2ceb23c98c95d52fce43",
+    "url": "/calculator/static/css/main.d798a55d.chunk.css"
+  },
+  {
+    "revision": "d489249f9840286d160b8cdbee84443e",
+    "url": "/calculator/index.html"
+  }
+];
